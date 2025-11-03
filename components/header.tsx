c392894746cd35cbd1004a6d9f8d9c67a78566cf
Alex <alex@Alexs-MacBook-Air.local>
2025-11-03 23:35:19 +0200

    added blur

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ export const Header = ({ onJoinClick }: { onJoinClick: () => void }) => {
   return (
     <div className="fixed z-50 pt-8 md:pt-14 top-0 left-0 w-full bg-gradient-to-b from-black/80 to-transparent pointer-events-none">
       <div className="container pointer-events-auto">
-        <header className="relative flex items-center justify-between bg-foreground/5 border border-foreground/10 rounded-lg px-4 md:px-6 py-3 md:py-4">
+        <header className="relative flex items-center justify-between bg-foreground/5 border border-foreground/10 rounded-lg px-4 md:px-6 py-3 md:py-4 backdrop-blur-md">
         <Link href="/">
           <Logo className="w-auto h-8 md:h-10" />
         </Link>
